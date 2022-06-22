7ba94e92a6b57e5f36582f05f8abde8e9edce9fc
Mohammed Nafees <hello@mnafees.me>
2022-06-23 00:46:59 +0530

    mehh

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+console.log("test");
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
