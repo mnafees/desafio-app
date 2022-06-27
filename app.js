1a8e7ac4ad734de27d95ac478739aced10a7168a
Mohammed Nafees <hello@mnafees.me>
2022-06-27 20:21:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-app.use(logger('dev'));
+app.use(logger('dev11'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
